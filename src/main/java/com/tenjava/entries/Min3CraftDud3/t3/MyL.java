36c72e229c291232a926c59a9b3220cdc4af1bcf
Min3CraftDud3 <hubeb93@gmail.com>
2014-07-12 16:30:48 -0400

    fixed broken code

--- src/main/java/com/tenjava/entries/Min3CraftDud3/t3/MyL.java
+++ src/main/java/com/tenjava/entries/Min3CraftDud3/t3/MyL.java
@@ -24,13 +24,14 @@ public class MyL implements Listener {
 
     @EventHandler
     public void onMovement(PlayerMoveEvent e){
+        Player p = e.getPlayer();
         if(encounterSteps==0){
             encounterSteps = rand.nextInt(400);
         }
         if(e.getFrom().getBlockX()==e.getTo().getBlockX() && e.getFrom().getBlockY()==e.getTo().getBlockY() && e.getFrom().getBlockZ()==e.getTo().getBlockZ())return;
         numSteps++;
         System.out.println(numSteps);
-        checkSteps(e.getPlayer());
+        checkSteps(p);
     }
     public static void checkSteps(Player p){
         if(numSteps==encounterSteps){
@@ -43,41 +44,37 @@ public class MyL implements Listener {
         int numMobs = rand.nextInt(6);
         Location mSpawnLoc;
         for (int i = 0; i < numMobs; i++) {
-            double locX = randInt((int)l.getX()-5,(int)l.getX()+5);
-            double locY = randInt((int)l.getY()-5,(int)l.getY()+5);
-            double locZ = w.getHighestBlockYAt((int)locX,(int)locY);
-            p.sendMessage(ChatColor.RED+"Wild Mobs Appeared!!");
-            int type = rand.nextInt(7);
-            switch (type) {
-                case 0: //Zombie
-                    mSpawnLoc = new Location(w,locX,locY,locZ);
-                    Bukkit.getWorld(p.getWorld().toString()).spawnEntity(mSpawnLoc, EntityType.ZOMBIE);
-                    break;
-                case 1: //Skeleton
-                    mSpawnLoc = new Location(w,locX,locY,locZ);
-                    Bukkit.getWorld(p.getWorld().toString()).spawnEntity(mSpawnLoc, EntityType.SKELETON);
-                    break;
-                case 2: //Spider
-                    mSpawnLoc = new Location(w,locX,locY,locZ);
-                    Bukkit.getWorld(p.getWorld().toString()).spawnEntity(mSpawnLoc, EntityType.SPIDER);
-                    break;
-                case 3: //Witch
-                    mSpawnLoc = new Location(w,locX,locY,locZ);
-                    Bukkit.getWorld(p.getWorld().toString()).spawnEntity(mSpawnLoc, EntityType.WITCH);
-                    break;
-                case 4: //CaveSpider
-                    mSpawnLoc = new Location(w,locX,locY,locZ);
-                    Bukkit.getWorld(p.getWorld().toString()).spawnEntity(mSpawnLoc, EntityType.CAVE_SPIDER);
-                    break;
-                case 5: //PigZombie
-                    mSpawnLoc = new Location(w,locX,locY,locZ);
-                    Bukkit.getWorld(p.getWorld().toString()).spawnEntity(mSpawnLoc, EntityType.PIG_ZOMBIE);
-                    break;
-                case 6: //Creeper
-                    mSpawnLoc = new Location(w,locX,locY,locZ);
-                    Bukkit.getWorld(p.getWorld().toString()).spawnEntity(mSpawnLoc, EntityType.CREEPER);
-                    break;
-            }
+            try {
+                double locX = randInt((int) l.getX() - 5, (int) l.getX() + 5);
+                double locY = randInt((int) l.getY() - 5, (int) l.getY() + 5);
+                double locZ = w.getHighestBlockYAt((int) locX, (int) locY);
+                mSpawnLoc = new Location(w, locX, locY, locZ);
+                p.sendMessage(ChatColor.RED + "Wild Mobs Appeared!!");
+                int type = rand.nextInt(6);
+                switch (type) {
+                    case 0: //Zombie
+                        Bukkit.getWorld(w.getName().toString()).spawnEntity(mSpawnLoc, EntityType.ZOMBIE);
+                        break;
+                    case 1: //Skeleton
+                        Bukkit.getWorld(w.getName().toString()).spawnEntity(mSpawnLoc, EntityType.SKELETON);
+                        break;
+                    case 2: //Spider
+                        Bukkit.getWorld(w.getName().toString()).spawnEntity(mSpawnLoc, EntityType.SPIDER);
+                        break;
+                    case 3: //Witch
+                        Bukkit.getWorld(w.getName().toString()).spawnEntity(mSpawnLoc, EntityType.WITCH);
+                        break;
+                    case 4: //CaveSpider
+                        Bukkit.getWorld(w.getName().toString()).spawnEntity(mSpawnLoc, EntityType.CAVE_SPIDER);
+                        break;
+                    case 5: //PigZombie
+                        Bukkit.getWorld(w.getName().toString()).spawnEntity(mSpawnLoc, EntityType.PIG_ZOMBIE);
+                        break;
+                    case 6: //Creeper
+                        Bukkit.getWorld(w.getName().toString()).spawnEntity(mSpawnLoc, EntityType.CREEPER);
+                        break;
+                }
+            }catch(Exception ig){ig.printStackTrace();}
             encounterSteps=0;
         }
     }
