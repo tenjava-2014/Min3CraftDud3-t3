49759d891b0db473ec5560ef71685fe3da7f7be9
Min3CraftDud3 <hubeb93@gmail.com>
2014-07-12 17:12:08 -0400

    5:12

--- src/main/java/com/tenjava/entries/Min3CraftDud3/t3/MyL.java
+++ src/main/java/com/tenjava/entries/Min3CraftDud3/t3/MyL.java
@@ -6,8 +6,10 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Slime;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import java.util.Random;
@@ -27,6 +29,7 @@ public class MyL implements Listener {
         Player p = e.getPlayer();
         if(encounterSteps==0){
             encounterSteps = rand.nextInt(400);
+            System.out.println(encounterSteps);
         }
         if(e.getFrom().getBlockX()==e.getTo().getBlockX() && e.getFrom().getBlockY()==e.getTo().getBlockY() && e.getFrom().getBlockZ()==e.getTo().getBlockZ())return;
         numSteps++;
@@ -36,6 +39,8 @@ public class MyL implements Listener {
     public static void checkSteps(Player p){
         if(numSteps==encounterSteps){
             doMobAttack(p);
+            p.sendMessage(ChatColor.RED + "Wild Mobs Appeared!!");
+            System.out.println("Spawning mobs on player");
         }
     }
     public static void doMobAttack(Player p) {
@@ -44,42 +49,54 @@ public class MyL implements Listener {
         int numMobs = rand.nextInt(6);
         Location mSpawnLoc;
         for (int i = 0; i < numMobs; i++) {
-            try {
                 double locX = randInt((int) l.getX() - 5, (int) l.getX() + 5);
                 double locY = randInt((int) l.getY() - 5, (int) l.getY() + 5);
                 double locZ = w.getHighestBlockYAt((int) locX, (int) locY);
                 mSpawnLoc = new Location(w, locX, locY, locZ);
-                p.sendMessage(ChatColor.RED + "Wild Mobs Appeared!!");
                 int type = rand.nextInt(6);
                 switch (type) {
                     case 0: //Zombie
-                        Bukkit.getWorld(w.getName().toString()).spawnEntity(mSpawnLoc, EntityType.ZOMBIE);
+                       // p.getWorld().spawnEntity(mSpawnLoc, EntityType.ZOMBIE);
+                        p.getWorld().spawnCreature(mSpawnLoc, EntityType.ZOMBIE);
+                        Bukkit.broadcastMessage("Z");
                         break;
                     case 1: //Skeleton
-                        Bukkit.getWorld(w.getName().toString()).spawnEntity(mSpawnLoc, EntityType.SKELETON);
+                        p.getWorld().spawnEntity(mSpawnLoc, EntityType.SKELETON);
+                        Bukkit.broadcastMessage("S");
                         break;
                     case 2: //Spider
-                        Bukkit.getWorld(w.getName().toString()).spawnEntity(mSpawnLoc, EntityType.SPIDER);
+                        p.getWorld().spawnEntity(mSpawnLoc, EntityType.SPIDER);
+                        Bukkit.broadcastMessage("SP");
                         break;
                     case 3: //Witch
-                        Bukkit.getWorld(w.getName().toString()).spawnEntity(mSpawnLoc, EntityType.WITCH);
+                        p.getWorld().spawnEntity(mSpawnLoc, EntityType.WITCH);
+                        Bukkit.broadcastMessage("W");
                         break;
                     case 4: //CaveSpider
-                        Bukkit.getWorld(w.getName().toString()).spawnEntity(mSpawnLoc, EntityType.CAVE_SPIDER);
+                        p.getWorld().spawnEntity(mSpawnLoc, EntityType.CAVE_SPIDER);
+                        Bukkit.broadcastMessage("CS");
                         break;
                     case 5: //PigZombie
-                        Bukkit.getWorld(w.getName().toString()).spawnEntity(mSpawnLoc, EntityType.PIG_ZOMBIE);
+                        p.getWorld().spawnEntity(mSpawnLoc, EntityType.PIG_ZOMBIE);
+                        Bukkit.broadcastMessage("PZ");
                         break;
                     case 6: //Creeper
-                        Bukkit.getWorld(w.getName().toString()).spawnEntity(mSpawnLoc, EntityType.CREEPER);
+                        p.getWorld().spawnEntity(mSpawnLoc, EntityType.CREEPER);
+                        Bukkit.broadcastMessage("C");
                         break;
                 }
-            }catch(Exception ig){ig.printStackTrace();}
-            encounterSteps=0;
         }
+        encounterSteps=0;
     }
     public static int randInt(int min, int max){
         int randNumber = rand.nextInt((max-min)+1)+min;
         return randNumber;
     }
+    @EventHandler
+    public void onSpawn(CreatureSpawnEvent e){
+        if(e.getEntity() instanceof Slime){
+            e.setCancelled(true);
+        }
+    }
+
 }
